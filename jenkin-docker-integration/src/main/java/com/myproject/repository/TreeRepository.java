376bd6a6dc9990f1688dac5295802da632a38e47
Swayam <Swayam@192.168.1.5>
2020-12-15 15:14:00 +0530

    For CI/CD (Git_Tomcat)

--- jenkin-docker-integration/src/main/java/com/myproject/repository/TreeRepository.java
+++ jenkin-docker-integration/src/main/java/com/myproject/repository/TreeRepository.java
@@ -2,7 +2,7 @@ package com.myproject.repository;
 
 import java.util.List;
 
-import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
